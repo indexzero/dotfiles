445c1feab2c5d5d44d593c3779f2a6eba1c2c08f
indexzero <charlie.robbins@gmail.com>
2013-12-02 00:10:32 -0800

    [dist] Added .private which only lives locally and not in git.

--- files/.profile
+++ files/.profile
@@ -9,7 +9,7 @@ done
 
 # Load ~/.bash_prompt, ~/.exports, ~/.aliases, ~/.functions and ~/.extra
 # ~/.extra can be used for settings you don’t want to commit
-for file in ~/.{exports,aliases,functions,bash_prompt,extra}; do
+for file in ~/.{exports,aliases,functions,bash_prompt,extra,private}; do
   [ -r "$file" ] && source "$file"
 done
 unset file
--- install.sh
+++ install.sh
@@ -9,25 +9,25 @@ function move_files () {
   sdir="$SCRIPT_PATH/$1"
   [ -z "$2" ] && tdir="$HOME" || tdir="$HOME/$2"
   [ -z "$2" ] && bdir="$HOME/.backup" || bdir="$HOME/.backup/$2"
-  
+
   echo "Installing $sdir into $tdir"
   echo "All existing files will be moved to $bdir"
-  
+
   if [ ! -z "$2" ]; then
     if [ -z $DRY_RUN ] && [ ! -d "$tdir" ]; then
       mkdir -p "$tdir"
     fi
-    
+
     if [ ! -d "$bdir" ]; then
       mkdir -p "$bdir"
     fi
   fi
-  
+
   for file in `ls -a $sdir | egrep "^\.\w|^\w"`; do
     if [ -f "$tdir/$file" ]; then
       cp -f "$tdir/$file" "$bdir/$file"
     fi
-    
+
     if [ -z $DRY_RUN ]; then
       echo "Installing $1/$file"
       cp -f "$sdir/$file" "$tdir/$file"
